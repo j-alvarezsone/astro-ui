e45020dd59452dc0e35d05374a88dc2c5360df88
Jorge Alvarez Sone <j.alvarezsone@gmail.com>
2026-04-10 19:29:01 +0200

    chore: add vitest

---
+++ vitest.config.ts
@@ -0,0 +1,32 @@
+import { fileURLToPath, URL } from 'node:url';
+
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      '@components': fileURLToPath(new URL('./src/components', import.meta.url)),
+      '@layouts': fileURLToPath(new URL('./src/layouts', import.meta.url)),
+      '@web-components': fileURLToPath(new URL('./src/web-components', import.meta.url)),
+      '@/types': fileURLToPath(new URL('./src/share/types', import.meta.url)),
+      '@utils': fileURLToPath(new URL('./src/share/utils', import.meta.url)),
+    },
+  },
+  test: {
+    environment: 'happy-dom',
+    include: ['src/**/*.{test,spec}.ts'],
+    reporters: ['verbose'],
+    coverage: {
+      provider: 'v8',
+      include: ['src/**/*.ts'],
+      exclude: ['src/**/*.{test,spec}.ts', 'src/**/*.d.ts'],
+      reporter: ['text', 'html', 'lcov'],
+      thresholds: {
+        lines: 80,
+        functions: 80,
+        branches: 80,
+        statements: 80,
+      },
+    },
+  },
+});
